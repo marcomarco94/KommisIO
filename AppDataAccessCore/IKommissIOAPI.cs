2509dbd062fb73bb4576ad89ecbce5f7d1987cdb
Sebastian Steininger <34867906+SteiningerSebastian@users.noreply.github.com>
2024-02-12 15:11:26 +0100

    Fix implementation of dummy, new roles

--- AppDataAccessCore/IKommissIOAPI.cs
+++ AppDataAccessCore/IKommissIOAPI.cs
@@ -14,7 +14,7 @@ namespace DataRepoCore {
         public Employee? CurrentEmployee { get; set; }
 
         /// <summary>
-        /// Identify an User by the personnel number and password.
+        /// Identify an User by the personnel number and password. (role: anonymous, default)
         /// </summary>
         /// <param name="personnelNumber">The number identifying the employee.</param>
         /// <param name="password">The password</param>
@@ -22,13 +22,13 @@ namespace DataRepoCore {
         public Task<Employee?> IdentifyAndAuthenticateAysnc(short personnelNumber, string password);
 
         /// <summary>
-        /// Get all currently open picking orders.
+        /// Get all currently open picking orders. (role: employee)
         /// </summary>
         /// <returns>Return an enumerable of picking orders.</returns>
         public Task<IEnumerable<PickingOrder>> GetOpenPickingOrdersAsync();
 
         /// <summary>
-        /// Pick an item from a stock position.
+        /// Pick an item from a stock position. (role: employee)
         /// </summary>
         /// <param name="orderPosition">The order-position for which to pick.</param>
         /// <param name="position">The stock-position from which to pick.</param>
@@ -37,58 +37,58 @@ namespace DataRepoCore {
         public Task<bool> PickAsync(PickingOrderPosition orderPosition, StockPosition position, int? amount = null);
 
         /// <summary>
-        /// Get all stock positions of an given article
+        /// Get all stock positions of an given article (role: employee, manager)
         /// </summary>
         /// <param name="article">The article for which all its stock positions are requested.</param>
         /// <returns></returns>
         public Task<IEnumerable<StockPosition>> GetStockPositionsForArticleAsync(Article article);
 
         /// <summary>
-        /// Assign an employee to a picking order.
+        /// Assign the employee to a picking order. (role: employee)
         /// </summary>
         /// <param name="order">The order to which an employee should be assigned</param>
         /// <returns>True if the employee was successfully assigned.</returns>
-        public Task<bool> AssignEmployeeToPickingOrderAsync(PickingOrder order);
+        public Task<bool> AssignToPickingOrderAsync(PickingOrder order);
 
         /// <summary>
-        /// Get all picking orders, even those which are allready assigned to an employee or completed.
+        /// Get all picking orders, even those which are allready assigned to an employee or completed. (role: manager, administrator)
         /// </summary>
         /// <returns>Return an enumerable of all picking-orders.</returns>
         public Task<IEnumerable<PickingOrder>> GetPickingOrdersAsync();
 
         /// <summary>
-        /// Get all picking orders that are finished.
+        /// Get all picking orders that are finished. (role: manager, administrator)
         /// </summary>
         /// <returns>Return an enumerable of all finished picking orders.</returns>
         public Task<IEnumerable<PickingOrder>> GetFinishedPickingOrdersAsync();
 
         /// <summary>
-        /// Get all picking orders that are in progress, a employee is assigned but items are still to be picked.
+        /// Get all picking orders that are in progress, a employee is assigned but items are still to be picked. (role: manager, administrator)
         /// </summary>
         /// <returns>Return an enumerable of all in progress picking orders.</returns>
         public Task<IEnumerable<PickingOrder>> GetInProgressPickingOrdersAsync();
 
         /// <summary>
-        /// Get all picking orders that are in progress and assigned to the current user.
+        /// Get all picking orders that are in progress and assigned to the current user. (role: employee)
         /// </summary>
         /// <returns>Return an enumerable of all in progress picking orders.</returns>
         public Task<IEnumerable<PickingOrder>> GetInProgressAssignedPickingOrdersAsync();
 
         /// <summary>
-        /// Reset the database to default.
+        /// Reset the database to default. (role: administrator)
         /// </summary>
         /// <returns>Returns true if successfull otherwise false.</returns>
         public Task<bool> ResetToDefaultAsync();
 
         /// <summary>
-        /// Report a damaged article.
+        /// Report a damaged article. (role: employee)
         /// </summary>
         /// <param name="report">The report to file.</param>
         /// <returns>True if the report was successfully filed.</returns>
         public Task<bool> ReportDamagedArticleAsync(DamageReport report);
 
         /// <summary>
-        /// Get all damage reports.
+        /// Get all damage reports. (role: manager)
         /// </summary>
         /// <returns>Return an enumerable of all damage-reports.</returns>
         public Task<IEnumerable<DamageReport>> GetArticleDamageReportsAsync();
